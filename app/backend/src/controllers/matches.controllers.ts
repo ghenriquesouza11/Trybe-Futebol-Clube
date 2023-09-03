d2e99396017ad682066a5beaab40dc43fb76dfc7
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-09-03 11:56:00 -0300

    feat: requisito 16

--- app/backend/src/controllers/matches.controllers.ts
+++ app/backend/src/controllers/matches.controllers.ts
@@ -4,8 +4,21 @@ import MatchesServices from '../services/matches.services';
 export default class MatchesControllers {
   constructor(private matchesServices: MatchesServices = new MatchesServices()) { }
 
-  async getAllMatches(_req: Request, res: Response): Promise<Response> {
+  async getAllMatches(req: Request, res: Response): Promise<Response> {
     const { status, data } = await this.matchesServices.getAllMatches();
+
+    const { inProgress } = req.query;
+
+    if (inProgress === 'true') {
+      const inProgressMatches = data.filter((match) => match.inProgress === true);
+
+      return res.status(status).json(inProgressMatches);
+    }
+    if (inProgress === 'false') {
+      const finishedMatches = data.filter((match) => match.inProgress === false);
+
+      return res.status(status).json(finishedMatches);
+    }
 
     return res.status(status).json(data);
   }
